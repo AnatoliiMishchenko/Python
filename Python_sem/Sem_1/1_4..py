fc990acb5a604363af4fbe5b35d0383eb29556c6
Anatolii Mishchenko <87mischenko@mail.ru>
2022-09-23 13:35:56 +0700

    Home work sem one

---
+++ Python_sem/Sem_1/1_4..py
@@ -0,0 +1,4 @@
+print('Введите число дробное')
+a = float(input())
+a = a*10 % 10
+print(int(a))
---
+++ Python_sem/Sem_1/1_5.py
@@ -0,0 +1,6 @@
+print('Введите число')
+a = int(input())
+if ((a % 5 == 0 and a % 10 == 0  or a % 15 == 0) and (a % 30 != 0)):
+    print('да')
+else:
+    print('нет')

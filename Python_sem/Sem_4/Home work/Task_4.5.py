34d64ccba5babf9a68af32e4d8fcbc521453e5c0
Anatolii Mishchenko <87mischenko@mail.ru>
2022-10-06 02:27:32 +0700

    Home work 4 sem

--- Python_sem/Sem_4/Home work/Task_4.5.py
+++ Python_sem/Sem_4/Home work/Task_4.5.py
@@ -14,3 +14,55 @@
 
 # out
 # The contents of the files do not match!
+
+from random import randint
+import random
+
+
+
+
+
+def mnogochlen(k):
+    with open("my_txt.txt", 'a', encoding="UTF-8") as t:
+        for i in range(k, -1, -1):
+            char_l = "+-"
+            B = random.choice(char_l)
+            A = random.randrange(0,10)
+            if A != 0:
+                if i == 0:
+                    t.write(f"{A} = 0 \n")
+                else:
+                    t.write(f"{A}*x^{i} {B}  ")
+            else:
+                if i == 0:
+                    t.write(" = 0\n")
+                
+
+
+for i in range(3):
+    k = int(input("Введите степень многочелена k "))
+    mnogochlen(k)
+    
+
+def mnogochlen(k):
+    with open("my_txt1.txt", 'a', encoding="UTF-8") as t:
+        for i in range(k, -1, -1):
+            char_l = "+-"
+            B = random.choice(char_l)
+            A = random.randrange(0,10)
+            if A != 0:
+                if i == 0:
+                    t.write(f"{A} = 0 \n")
+                else:
+                    t.write(f"{A}*x^{i} {B}  ")
+            else:
+                if i == 0:
+                    t.write(" = 0\n")
+                
+
+
+for i in range(3):
+    k = int(input("Введите степень многочелена k2 "))
+    mnogochlen(k)
+
+open("file3.txt","w").write(open("my_txt1.txt","r").read() + open("my_txt.txt","r").read())
